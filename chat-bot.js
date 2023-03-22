647c4aeff7b0e4f4d133f483d37dd5a7a8341a7a
kirurr <timon09df@gmail.com>
2023-03-22 20:00:59 +0300

    йуйуййууй

---
+++ chat-bot.js
@@ -0,0 +1,1 @@
+!function(e,t,a){var c=e.head||e.getElementsByTagName("head")[0],n=e.createElement("script");n.async=!0,n.defer=!0, n.type="text/javascript",n.src=t+"/static/js/widget.js?config="+JSON.stringify(a),c.appendChild(n)}(document,"https://app.engati.com",{bot_key:"2fbb5c1c51094f81",welcome_msg:true,branding_key:"default",server:"https://app.engati.com",e:"p" });
